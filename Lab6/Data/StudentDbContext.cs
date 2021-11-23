5732b731cc041ae93b13ca9e2940885855eb1dad
Andrew Jajoyacoub <andrewjy1@hotmail.com>
2021-11-23 18:19:03 -0500

    Lab6 submission

---
+++ Lab6/Data/StudentDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab6.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab6.Data
+{
+    public class StudentDbContext : DbContext
+    {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+
+    }
+}
---
+++ Lab6/Models/StudentBase.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab6.Models
+{
+    public class StudentBase
+    {
+        [StringLength(50, MinimumLength = 1)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(50, MinimumLength =1)]
+        [Required]
+        public string LastName { get; set; }  
+
+        [StringLength(50, MinimumLength =1)]
+        [Required]
+        public string Program { get; set; }
+    }
+}
